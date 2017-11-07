b0cb903745966af43d18e9f3b1b36768193a7408
Richard Melo <allucardster@gmail.com>
2017-11-07 00:55:35 -0500

    Update from 2.8.x to 3.3.x

--- web/app_dev.php
+++ web/app_dev.php
@@ -23,7 +23,8 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+//$kernel->loadClassCache();
+//Request::setTrustedProxies([]);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
