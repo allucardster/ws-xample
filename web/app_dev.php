76ec2896533ca719d49d00a43b22b5175f2f07d9
Richard Melo <allucardster@gmail.com>
2017-11-13 23:18:52 -0500

    Update directory structure from 2.8.x to 3.3.x

--- web/app_dev.php
+++ web/app_dev.php
@@ -21,7 +21,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
-
+require_once __DIR__.'/../app/AppKernel.php';
 $kernel = new AppKernel('dev', true);
 //$kernel->loadClassCache();
 //Request::setTrustedProxies([]);
