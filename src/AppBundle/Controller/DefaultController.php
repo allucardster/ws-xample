72ffdfd4c30f03495be7ae952ac2fd3973f7d18d
Richard Melo <allucardster@gmail.com>
2017-11-20 01:43:02 -0500

    Add chat UI and update the base templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,5 +7,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
-{   
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->redirectToRoute('chat');
+    }
+
+    /**
+     * @Route("/chat", name="chat")
+     * @Route("/chat/{all}", name="chat-all", requirements={"all": ".+"})
+     */
+    public function chatAction(Request $request)
+    {
+        return $this->render('default/chat.html.twig', []);
+    }
 }
